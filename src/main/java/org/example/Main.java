ec2d6767810d22fa88f38bc1cae62c150c5a23a3
Ayman Khafifi <khafifiayman318@gmail.com>
2026-05-19 08:31:45 +0100

    context started

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Context starting...");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        System.out.println("Context started");
     }
 }
---
+++ src/main/java/org/example/StudentService.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+public class StudentService {
+
+}
